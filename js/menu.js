72d9eb962e3105178d6f82b380adcfe88fe0d0f6
Will Taylor <taylorw@udel.edu>
2017-01-26 05:55:18 -0500

    Added lotsa assets peep em fam

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,76 @@
 
 var menuState = {
-
 	create: function() {
 		game.add.tileSprite(0, 0, 1000, 600, 'bg');
 
-		//var nameLabel1 = game.add.text(80, 40, 'MENU',
-			//{font: '25px Arial', fill: '#ffffff' });
 		var title = game.add.sprite(game.width/2 - game.cache.getImage('title').width/2 ,80, 'title');
-		//var nameLabel2 = game.add.text(80, 120, 'Press \'w\' to go to the game.', 
-			//{font: '25px Arial', fill: '#ffffff' });
 
-		var howtoplay_button = game.add.button(game.width/2 - game.cache.getImage('how_to_play').width/6, 330, 'how_to_play', this.instructions, this, 0, 1, 2);
-		var start_button = game.add.button(game.width/2 - game.cache.getImage('start').width/6, 230, 'start', this.start, this, 0, 1, 2);
+		this.howtoplay_button = game.add.button(game.width/2 - game.cache.getImage('how_to_play').width/6, 330, 'how_to_play', this.instructions, this, 0, 1, 2);
+		this.start_button = game.add.button(game.width/2 - game.cache.getImage('start').width/6, 230, 'start', this.start, this, 0, 1, 2);
+		this.sim_sprite = game.add.sprite(game.width/2 - game.cache.getImage('title').width/2 , 175, 'sim_title')
 
-		//var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-		//wKey.onDown.addOnce(this.start, this);
-		//var iKey = game.input.keyboard.addKey(Phaser.Keyboard.I);
-		//iKey.onDown.addOnce(this.instructions, this);
+			this.preTrain_1_button = game.add.button((game.width/2)-150, game.height/2+20, 'preTrain1', function(){
+				simGames = 1;
+				this.play();
+			}, this, 1, 0, 2);
+			this.preTrain_10_button = game.add.button(game.width/2-50, game.height/2+20, 'preTrain10', function(){
+				simGames = 10;
+				this.play();
+			}, this, 1, 0, 2);
+			this.preTrain_100_button = game.add.button(game.width/2+50, game.height/2+20, 'preTrain100', function(){
+				simGames = 100;
+				this.play();
+			}, this, 1, 0, 2);
 
+
+			this.preTrain_1_button.visible = false;
+			this.preTrain_10_button.visible = false;
+			this.preTrain_100_button.visible = false;
+			this.sim_sprite.visible = false;
 	},
 
 	start: function() {
-		simGames = 100;
-		game.state.start('preplay');
+		this.start_button.visible = false;
+		this.sim_sprite.visible = true;
+		this.howtoplay_button.visible = false;
+		this.preTrain_1_button.visible = true;
+		this.preTrain_10_button.visible = true;
+		this.preTrain_100_button.visible = true;
 	},
 
 	instructions: function(){
 		game.state.start('howtoplay');
 	},
 
+	train1: function(){
+		var startTime = new Date().getTime() / 1000;
+			ai.trainAI(1);
+			var endTime = new Date().getTime() / 1000;
+			var simTime = endTime - startTime;
+			Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
+			Logger.debug('Done w simulation');
+
+	},
+	train10: function(){
+		var startTime = new Date().getTime() / 1000;
+			ai.trainAI(10);
+			var endTime = new Date().getTime() / 1000;
+			var simTime = endTime - startTime;
+			Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
+			Logger.debug('Done w simulation');
+
+	},
+	train100: function(){
+		var startTime = new Date().getTime() / 1000;
+			ai.trainAI(100);
+			var endTime = new Date().getTime() / 1000;
+			var simTime = endTime - startTime;
+			Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
+			Logger.debug('Done w simulation');
+
+	},
+
+	play: function(){
+		game.state.start('play');
+	}
 };
--- js/preplay.js
+++ js/preplay.js
@@ -1,54 +1,11 @@
 var preplayState = {
 
 	create: function(){
-		var header = game.add.text(40, 40, 'Pre Training', 
+		var header = game.add.text(40, 40, 'Pre Training',
 			{font: '50px Arial', fill: '#ffffff' });
 
-		start_button = game.add.button(game.width/2-50, game.height-50, 'button_start', function(){
-	      this.play();
-	    }, this, 0, 1, 2);
-	    preTrain_1_button = game.add.button((game.width/4)-100, game.height/2, 'preTrain1', function(){
-	      simGames = 1;
-	    }, this, 0, 1, 2);
-	    preTrain_10_button = game.add.button(game.width/4, game.height/2, 'preTrain10', function(){
-	      simGames = 10;
-	    }, this, 0, 1, 2);
-	    preTrain_100_button = game.add.button((game.width/4)+100, game.height/2, 'preTrain100', function(){
-	      simGames = 100;
-	    }, this, 0, 1, 2);
+
 
 	},
-
-	train1: function(){
-		var startTime = new Date().getTime() / 1000;
-  		ai.trainAI(1);
-  		var endTime = new Date().getTime() / 1000;
-  		var simTime = endTime - startTime;
-  		Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
-  		Logger.debug('Done w simulation');
-
-	},
-	train10: function(){
-		var startTime = new Date().getTime() / 1000;
-  		ai.trainAI(10);
-  		var endTime = new Date().getTime() / 1000;
-  		var simTime = endTime - startTime;
-  		Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
-  		Logger.debug('Done w simulation');
-
-	},
-	train100: function(){
-		var startTime = new Date().getTime() / 1000;
-  		ai.trainAI(100);
-  		var endTime = new Date().getTime() / 1000;
-  		var simTime = endTime - startTime;
-  		Logger.info('%c Simulation took ' + simTime + ' seconds', 'background: #222; color: #fff');
-  		Logger.debug('Done w simulation');
-
-	},
-
-	play: function(){
-		game.state.start('play');
-	}
 
 };

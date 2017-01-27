5e0f0410d20d31a26c694bf2a9b8e99db0dd57ff
ldawson02 <ldawson02@yahoo.com>
2017-01-27 19:24:48 +1300

    dumb changes

--- js/menu.js
+++ js/menu.js
@@ -9,24 +9,24 @@ var menuState = {
 		this.start_button = game.add.button(game.width/2 - game.cache.getImage('start').width/6, 230, 'start', this.start, this, 0, 1, 2);
 		this.sim_sprite = game.add.sprite(game.width/2 - game.cache.getImage('title').width/2 , 175, 'sim_title')
 
-			this.preTrain_1_button = game.add.button((game.width/2)-50, game.height/2+20, 'preTrain1', function(){
-				simGames = 1;
-				this.play();
-			}, this, 1, 0, 2);
-			this.preTrain_10_button = game.add.button(game.width/2-50, game.height/2+80, 'preTrain10', function(){
-				simGames = 10;
-				this.play();
-			}, this, 1, 0, 2);
-			this.preTrain_100_button = game.add.button(game.width/2-50, game.height/2+140, 'preTrain100', function(){
-				simGames = 100;
-				this.play();
-			}, this, 1, 0, 2);
+		this.preTrain_1_button = game.add.button((game.width/2)-50, game.height/2+20, 'preTrain1', function(){
+			simGames = 1;
+			this.play();
+		}, this, 1, 0, 2);
+		this.preTrain_10_button = game.add.button(game.width/2-50, game.height/2+80, 'preTrain10', function(){
+			simGames = 10;
+			this.play();
+		}, this, 1, 0, 2);
+		this.preTrain_100_button = game.add.button(game.width/2-50, game.height/2+140, 'preTrain100', function(){
+			simGames = 100;
+			this.play();
+		}, this, 1, 0, 2);
 
 
-			this.preTrain_1_button.visible = false;
-			this.preTrain_10_button.visible = false;
-			this.preTrain_100_button.visible = false;
-			this.sim_sprite.visible = false;
+		this.preTrain_1_button.visible = false;
+		this.preTrain_10_button.visible = false;
+		this.preTrain_100_button.visible = false;
+		this.sim_sprite.visible = false;
 	},
 
 	start: function() {

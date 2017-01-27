4e4b876302a47da53e1ba9d74b56e58dac1a4198
ldawson02 <ldawson02@yahoo.com>
2017-01-27 20:21:56 +1300

    fixing visuals

--- js/load.js
+++ js/load.js
@@ -10,9 +10,10 @@ var loadState = {
 		game.load.spritesheet('how_to_play', 'assets/buttons/howtoplay_button.png', 150, 75);
 		game.load.spritesheet('start', 'assets/buttons/start_button.png', 150, 75);
 		game.load.spritesheet('menu', 'assets/buttons/menu_button.png', 150, 75);
-		game.load.spritesheet('preTrain1', 'assets/buttons/sim_1.png', 100, 60);
-		game.load.spritesheet('preTrain10', 'assets/buttons/sim_2.png', 100, 60);
-		game.load.spritesheet('preTrain100', 'assets/buttons/sim_3.png', 100, 60);
+		game.load.spritesheet('preTrain0', 'assets/buttons/sim_0.png', 100, 50);
+		game.load.spritesheet('preTrain50', 'assets/buttons/sim_50.png', 100, 50);
+		game.load.spritesheet('preTrain100', 'assets/buttons/sim_100.png', 100, 50);
+		game.load.spritesheet('preTrain500', 'assets/buttons/sim_500.png', 100, 50);
 
 		game.load.image('stick_key', 'assets/sprites/stick.png');
 		game.load.image('slider', 'assets/sprites/slider.png');
@@ -23,6 +24,7 @@ var loadState = {
 		game.load.image('sim10', 'assets/buttons/button_simulate.png');
 		game.load.image('numsticks_slider', 'assets/sprites/numsticks_slider.png');
 		game.load.image('numSticks_arrow', 'assets/sprites/numSticks_arrow.png');
+		game.load.image('sticks_dialogue', 'assets/sprites/sticks_dialogue.png');
 	},
 
 	create: function() {
--- js/menu.js
+++ js/menu.js
@@ -7,47 +7,55 @@ var menuState = {
 
 		this.howtoplay_button = game.add.button(game.width/2 - game.cache.getImage('how_to_play').width/6, 330, 'how_to_play', this.instructions, this, 0, 1, 2);
 		this.start_button = game.add.button(game.width/2 - game.cache.getImage('start').width/6, 230, 'start', this.start, this, 0, 1, 2);
-		this.sim_sprite = game.add.sprite(game.width/2 - game.cache.getImage('title').width/2 , 175, 'sim_title')
+		this.sim_sprite = game.add.sprite(game.width/2 - game.cache.getImage('sim_title').width/2 , 500 - (game.cache.getImage('sim_title').height), 'sim_title');
 
-			this.preTrain_1_button = game.add.button((game.width/2)-50, game.height/2+20, 'preTrain1', function(){
-				simGames = 1;
+			this.preTrain_0_button = game.add.button((game.width/2)-175, 500, 'preTrain0', function(){
+				simGames = 0;
 				this.play();
-			}, this, 1, 0, 2);
-			this.preTrain_10_button = game.add.button(game.width/2-50, game.height/2+80, 'preTrain10', function(){
-				simGames = 10;
+			}, this, 0, 1, 2);
+			this.preTrain_50_button = game.add.button(game.width/2-75, 500, 'preTrain50', function(){
+				simGames = 50;
 				this.play();
-			}, this, 1, 0, 2);
-			this.preTrain_100_button = game.add.button(game.width/2-50, game.height/2+140, 'preTrain100', function(){
+			}, this, 0, 1, 2);
+			this.preTrain_100_button = game.add.button(game.width/2+25, 500, 'preTrain100', function(){
 				simGames = 100;
 				this.play();
-			}, this, 1, 0, 2);
+			}, this, 0, 1, 2);
+			this.preTrain_500_button = game.add.button(game.width/2+125, 500, 'preTrain500', function(){
+				simGames = 500;
+				this.play();
+			}, this, 0, 1, 2);
 
-
-			this.preTrain_1_button.visible = false;
-			this.preTrain_10_button.visible = false;
+			this.preTrain_0_button.visible = false;
+			this.preTrain_50_button.visible = false;
 			this.preTrain_100_button.visible = false;
+			this.preTrain_500_button.visible = false;
 			this.sim_sprite.visible = false;
 
-		this.numSticks_slider = game.add.sprite(10,500, 'numsticks_slider');
-    	numSticks_arrow = game.add.sprite(350,500, 'numSticks_arrow');
+		this.sticks_dialogue = game.add.sprite(game.width/2 - game.cache.getImage('sticks_dialogue').width/2 , game.height/2 + 20 - (game.cache.getImage('sticks_dialogue').height), 'sticks_dialogue');
+		this.numSticks_slider = game.add.sprite(game.width/2-game.cache.getImage('numsticks_slider').width/2, game.height/2-20, 'numsticks_slider');
+    	numSticks_arrow = game.add.sprite(game.width/2-game.cache.getImage('numsticks_slider').width/2 + 340, game.height/2-20, 'numSticks_arrow');
     	numSticks_arrow.inputEnabled = true;
-    	boundingBox = new Phaser.Rectangle(0, 500, 400, 50);
+    	boundingBox = new Phaser.Rectangle(game.width/2-game.cache.getImage('numsticks_slider').width/2, game.height/2+20, 400, 50);
     	numSticks_arrow.input.enableDrag(false, false, false, 255, boundingBox, null);
     	numSticks_arrow.input.setDragLock(true, true);
     	numSticks_arrow.input.enableSnap(50, 50, true, true);
 
     	this.numSticks_slider.visible = false;
 		numSticks_arrow.visible = false;
+		this.sticks_dialogue.visible = false;
 	},
 
 	start: function() {
 		this.start_button.visible = false;
 		this.sim_sprite.visible = true;
 		this.howtoplay_button.visible = false;
-		this.preTrain_1_button.visible = true;
-		this.preTrain_10_button.visible = true;
+		this.preTrain_0_button.visible = true;
+		this.preTrain_50_button.visible = true;
 		this.preTrain_100_button.visible = true;
-		
+		this.preTrain_500_button.visible = true;
+		this.sticks_dialogue.visible = true;
+
 		this.numSticks_slider.visible = true;
 		numSticks_arrow.visible = true;
 		console.log(numSticks_arrow.x);

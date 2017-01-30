744f90e4e504ee3b7c993e7581fae21c4fc41bbf
Greg Mohler <gregmohler@Gregs-MacBook-Pro-2.local>
2017-01-31 12:26:19 +1300

    slight formatting changes to menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,28 +9,28 @@ var menuState = {
 		this.start_button = game.add.button(game.width/2 - game.cache.getImage('start').width/6, 230, 'start', this.start, this, 0, 1, 2);
 		this.sim_sprite = game.add.sprite(game.width/2 - game.cache.getImage('sim_title').width/2 , 500 - (game.cache.getImage('sim_title').height), 'sim_title');
 
-			this.preTrain_0_button = game.add.button((game.width/2)-175, 500, 'preTrain0', function(){
-				simGames = 0;
-				this.play();
-			}, this, 0, 1, 2);
-			this.preTrain_50_button = game.add.button(game.width/2-75, 500, 'preTrain50', function(){
-				simGames = 50;
-				this.play();
-			}, this, 0, 1, 2);
-			this.preTrain_100_button = game.add.button(game.width/2+25, 500, 'preTrain100', function(){
-				simGames = 100;
-				this.play();
-			}, this, 0, 1, 2);
-			this.preTrain_500_button = game.add.button(game.width/2+125, 500, 'preTrain500', function(){
-				simGames = 500;
-				this.play();
-			}, this, 0, 1, 2);
+		this.preTrain_0_button = game.add.button((game.width/2)-175, 500, 'preTrain0', function(){
+			simGames = 0;
+			this.play();
+		}, this, 0, 1, 2);
+		this.preTrain_50_button = game.add.button(game.width/2-75, 500, 'preTrain50', function(){
+			simGames = 50;
+			this.play();
+		}, this, 0, 1, 2);
+		this.preTrain_100_button = game.add.button(game.width/2+25, 500, 'preTrain100', function(){
+			simGames = 100;
+			this.play();
+		}, this, 0, 1, 2);
+		this.preTrain_500_button = game.add.button(game.width/2+125, 500, 'preTrain500', function(){
+			simGames = 500;
+			this.play();
+		}, this, 0, 1, 2);
 
-			this.preTrain_0_button.visible = false;
-			this.preTrain_50_button.visible = false;
-			this.preTrain_100_button.visible = false;
-			this.preTrain_500_button.visible = false;
-			this.sim_sprite.visible = false;
+		this.preTrain_0_button.visible = false;
+		this.preTrain_50_button.visible = false;
+		this.preTrain_100_button.visible = false;
+		this.preTrain_500_button.visible = false;
+		this.sim_sprite.visible = false;
 
 		//this.sticks_dialogue = game.add.sprite(game.width/2 - game.cache.getImage('sticks_dialogue').width/2 , game.height/2 + 20 - (game.cache.getImage('sticks_dialogue').height), 'sticks_dialogue');
 		//this.numSticks_slider = game.add.sprite(game.width/2-game.cache.getImage('numsticks_slider').width/2, game.height/2-20, 'numsticks_slider');
